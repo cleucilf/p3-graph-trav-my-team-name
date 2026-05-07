a1d6f56e9617bdf0aa39884ff8d7fc567c060454
Cleucil Francisco <cleucilf@gmail.com>
2026-05-06 19:39:36 -0700

    Build assignment graph in main program

---
+++ src/main.java
@@ -0,0 +1,40 @@
+public class main
+{
+    public static void main(String[] args)
+    {
+        DirectedGraph<String> graph = new DirectedGraph<>();
+
+        // Add vertices
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
+        graph.addVertex("E");
+        graph.addVertex("F");
+        graph.addVertex("G");
+        graph.addVertex("H");
+        graph.addVertex("I");
+
+        // Add directed edges
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "D");
+        graph.addEdge("A", "E");
+        graph.addEdge("B", "E");
+        graph.addEdge("D", "G");
+        graph.addEdge("E", "F");
+        graph.addEdge("E", "H");
+        graph.addEdge("G", "H");
+        graph.addEdge("F", "C");
+        graph.addEdge("F", "H");
+        graph.addEdge("H", "I");
+        graph.addEdge("C", "B");
+        graph.addEdge("I", "F");
+
+        System.out.println("Directed Graph:");
+        graph.displayGraph();
+
+        System.out.println();
+        System.out.println("Number of vertices: " + graph.getNumberOfVertices());
+        System.out.println("Number of edges: " + graph.getNumberOfEdges());
+    }
+}
